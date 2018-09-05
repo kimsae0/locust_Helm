6b15972408321215d2ec481c4686ff7371a927b2
KSY <ksy@KSY-MacBook-Pro.local>
2018-09-05 17:12:01 +0900

    update task

--- docker/locust-tasks/tasks.py
+++ docker/locust-tasks/tasks.py
@@ -1,11 +1,29 @@
-from locust import HttpLocust, TaskSet, TaskSet
-
-class UserBehavior(TaskSet):
-    @task(4)
-    def testTask(self):
+import base64
+ 
+from locust import HttpLocust, TaskSet, task
+from random import randint, choice
+ 
+ 
+class WebTasks(TaskSet):
+ 
+    @task
+    def load(self):
+        base64_str = base64.encodestring(('%s:%s' % ('user','password')).encode()).decode().replace('\n', '')
+        catalogue = self.client.get("/catalogue").json()
+        category_item = choice(catalogue)
+        item_id = category_item["id"]
+ 
         self.client.get("/")
-
-class WebsiteUser(HttpLocust):
-    task_set = UserBehavior
-    min_wait = 1000
-    max_wait = 2000
+        self.client.get("/login", headers={"Authorization":"Basic %s" % base64_str})
+        self.client.get("/category.html")
+        self.client.get("/detail.html?id={}".format(item_id))
+        self.client.delete("/cart")
+        self.client.post("/cart", json={"id": item_id, "quantity": 1})
+        self.client.get("/basket.html")
+        self.client.post("/orders")
+ 
+ 
+class Web(HttpLocust):
+    task_set = WebTasks
+    min_wait = 500
+    max_wait = 1000
